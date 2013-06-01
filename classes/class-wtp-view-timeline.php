47442a6bfa6d332afb48b3d15cc2ac309a5f835f
Christopher Cochran <chris.cochran@get10up.com>
2013-06-01 10:47:47 -0400

    timeline

--- classes/class-wtp-view-timeline.php
+++ classes/class-wtp-view-timeline.php
@@ -42,6 +42,9 @@ class WTP_View_Timeline {
 
 		// enqueue the stylesheet for this view
 		wp_enqueue_style( 'wtp-view-timeline', WTP_Core::$plugins_url . '/css/wtp-view-timeline.css' );
+
+		wp_enqueue_script( 'chart-js', WTP_Core::$plugins_url . '/js/chart/chart.js' );
+
 	}
 
 	/**
@@ -52,7 +55,100 @@ class WTP_View_Timeline {
 	 * @return string
 	 */
 	public static function render( $post = false, $attributes = array() ) {
-		return '';
+
+		$days = array();
+		$weeks = array();
+		$signature_counts = array();
+		$signature_totals = array();
+
+		$petition_id = get_post_meta( $post->ID, 'petition_id', true );
+
+		$default_interval = 'daily';
+		$possible_intervals = array( 'daily', 'weekly', 'monthly' );
+
+		$default_show = 'total';
+		$possible_show = array( 'total', 'count', 'both' );
+
+		$default_attrinbutes = array(
+			'interval' => 'daily',
+			'show' 	   => 'total',
+			'type'     => 'line',
+		);
+
+		$attributes = wp_parse_args( $attributes, $default_attrinbutes );
+
+		if( ! in_array( $attributes['interval'], $possible_intervals ) )
+			$interval = $default_interval;
+
+		if( ! in_array( $attributes['show'], $possible_show ) )
+			$show = $default_show;
+
+		// get the geographic petition data
+		$timeline_data_array = WTP_Intermediary_API::get_petition_timeline_data( $petition_id, $attributes['interval'] );
+
+		$signature_count = 0;
+
+		if ( ! $timeline_data )
+			return;
+
+		foreach ( $timeline_data_array as $timeline_entry ) {
+
+
+			if ( "total" == $attributes['show'] || "both" == $attributes['show'] )
+				$signature_totals[] = $signature_count += $timeline_entry['count'];
+
+			if ( "count" == $attributes['show'] || "both" == $attributes['show'] )
+				$signature_counts[] = $timeline_entry['count'];
+
+			if ( isset( $timeline_entry['timestamp'] ) )
+				$days[] = date( 'm \/ d', $timeline_entry['timestamp'] );
+
+			if ( isset( $timeline_entry['start'] ) && isset( $timeline_entry['end'] ) )
+				$weeks[] = date( 'm \/ d', $timeline_entry['start'] ) . ' - ' . date( 'm \/ d', $timeline_entry['end'] );
+
+		}
+
+		$signature_totals = implode( ', ', $signature_totals );
+		$signature_counts = implode( ', ', $signature_counts );
+
+		$days = implode( '", "', $days );
+		$weeks = implode( '", "', $weeks );
+
+		$dates = ( $weeks ) ? '"' . $weeks . '"' : '"' . $days . '"';
+
+		ob_start();
+		?>
+		<canvas id="petition-timeline-<?php echo $petition_id . '-' . $attributes['interval']; ?>" width="<?php echo $attributes['width']; ?>" height="<?php echo $attributes['height']; ?>"></canvas>
+		<script>
+			var lineChartData = {
+				labels : [<?php echo $dates; ?>],
+				datasets : [
+					{
+						fillColor : "rgba(220,220,220,0.5)",
+						strokeColor : "rgba(220,220,220,1)",
+						pointColor : "rgba(220,220,220,1)",
+						pointStrokeColor : "#fff",
+						data : [<?php echo $signature_totals; ?>]
+					},
+					{
+						fillColor : "rgba(151,187,205,0.5)",
+						strokeColor : "rgba(151,187,205,1)",
+						pointColor : "rgba(151,187,205,1)",
+						pointStrokeColor : "#fff",
+						data : [<?php echo $signature_counts; ?>]
+					}
+				]
+
+			}
+
+			var myLine = new Chart(document.getElementById("petition-timeline-<?php echo $petition_id . '-' . $attributes['interval']; ?>").getContext("2d")).Line(lineChartData);
+
+		</script>
+		<?php
+		$html = ob_get_clean();
+		$html = apply_filters( 'wtp-view-timeline', $html );
+
+		return $html;
 	}
 
 }
