7b52c29fd993208b443c678be709652cd1400367
Carl Danley <carldanley@gmail.com>
2013-06-01 15:14:52 -0400

    added transients to get cache support

--- classes/class-wtp-intermediary-api.php
+++ classes/class-wtp-intermediary-api.php
@@ -37,16 +37,59 @@ class WTP_Intermediary_API {
 	}
 
 	/**
+	 * Retrieves the URL with caching involved. Checks the hourly key & degrades to daily key when not accessible.
+	 *
+	 * @param string $url
+	 * @return bool|mixed
+	 */
+	protected static function _retrieve_url_with_caching( $url = '' ) {
+		if( empty( $url ) )
+			return false;
+
+		$key = md5( $url );
+		$hourly_key = $key . '-hourly';
+		$daily_key = $key . '-daily';
+
+		// try getting the hourly key
+		$data = get_transient( $hourly_key );
+
+		if( ! $data ) {
+			// hourly key failed, try grabbing again
+			$data = self::_retrieve_url( $url );
+			if( ! $data ) {
+				// hourly key didn't exist and API failed, check the daily key as a last result
+				$data = get_transient( $daily_key );
+
+				if( ! $data )
+					return false;
+
+				return $data;
+			}
+
+			// reset transients
+			set_transient( $hourly_key, $data, 3600 );
+			set_transient( $daily_key, $data, 86400 );
+		}
+
+		return $data;
+	}
+
+	/**
 	 * Gets the petition data for the specified petition
 	 *
 	 * @param string $id
+	 * @param bool $use_cache
 	 * @return mixed
 	 */
-	public static function get_petition_data( $id = '' ) {
+	public static function get_petition_data( $id = '', $use_cache = true ) {
 		$params = array(
 			'id' => $id
 		);
 		$url = self::$_base_url . '?' . http_build_query( $params );
+
+		if( $use_cache )
+			return self::_retrieve_url_with_caching( $url );
+
 		return self::_retrieve_url( $url );
 	}
 
@@ -54,12 +97,17 @@ class WTP_Intermediary_API {
 	 * Searches the intermediary API for petitions with a like %% query
 	 *
 	 * @param array $params
+	 * @param bool $use_cache
 	 * @return mixed
 	 */
-	public static function search_petitions( $params = array() ) {
+	public static function search_petitions( $params = array(), $use_cache = true ) {
 		$params[ 'type' ] = 'search';
 
 		$url = self::$_base_url . '?' . http_build_query( $params );
+
+		if( $use_cache )
+			return self::_retrieve_url_with_caching( $url );
+
 		return self::_retrieve_url( $url );
 	}
 
@@ -67,15 +115,20 @@ class WTP_Intermediary_API {
 	 * Gets the geographical data for the specified petition
 	 *
 	 * @param string $id
+	 * @param bool $use_cache
 	 * @return mixed
 	 */
-	public static function get_petition_geographical_data( $id = '' ) {
+	public static function get_petition_geographical_data( $id = '', $use_cache = true ) {
 		$params = array(
 			'id' => $id,
 			'type' => 'geographical'
 		);
 
 		$url = self::$_base_url . '?' . http_build_query( $params );
+
+		if( $use_cache )
+			return self::_retrieve_url_with_caching( $url );
+
 		return self::_retrieve_url( $url );
 	}
 
@@ -86,9 +139,10 @@ class WTP_Intermediary_API {
 	 * @param string $interval
 	 * @param int $start
 	 * @param int $end
+	 * @param bool $use_cache
 	 * @return mixed
 	 */
-	public static function get_petition_timeline_data( $id = '', $interval = 'daily', $start = 0, $end = 0 ) {
+	public static function get_petition_timeline_data( $id = '', $interval = 'daily', $start = 0, $end = 0, $use_cache = true ) {
 		$default_interval = 'daily';
 		$possible_intervals = array( 'daily', 'weekly', 'monthly' );
 		if( ! in_array( $interval, $possible_intervals ) )
@@ -106,6 +160,10 @@ class WTP_Intermediary_API {
 			$params[ 'end' ] = $end;
 
 		$url = self::$_base_url . '?' . http_build_query( $params );
+
+		if( $use_cache )
+			return self::_retrieve_url_with_caching( $url );
+
 		return self::_retrieve_url( $url );
 	}
 
